a1a0e6f1618931877c36904fde27c833b6910de8
tewe <leventewe@gmail.com>
2024-10-27 13:06:07 +0100

    torrent

--- hosts/tewenixsrv/default.nix
+++ hosts/tewenixsrv/default.nix
@@ -10,6 +10,7 @@
       ../../modules/system.nix
       # ../../modules/i3.nix
       ../../modules/plex.nix
+      ../../modules/torrent.nix
 
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
