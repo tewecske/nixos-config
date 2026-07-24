ba7a0eaf7569c8728722f098bd948a3470d3abe1
tewe <leventewe@gmail.com>
2026-07-24 15:36:24 +0200

    Samba

--- modules/samba.nix
+++ modules/samba.nix
@@ -11,18 +11,17 @@
         "server string" = "tewenixsrv";
         "netbios name" = "tewenixsrv";
         "security" = "user";
-        "guest account" = "nobody";
-        "map to guest" = "bad user";
       };
 
       externalwd = {
         path = "/mnt/externalwd";
         browseable = true;
         "read only" = true;
-        "guest ok" = true;
-        "force user" = "nobody";
-        "force group" = "nogroup";
+        "valid users" = "tewe";
       };
     };
   };
+
+  services.samba-wsdd.enable = true;
 }
+
