b803bd1d365132125359cf6e11946d045ed56c88
tewe <leventewe@gmail.com>
2026-08-28 23:33:10 +0200

    snacks picker keys

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -99,6 +99,8 @@ snacks.setup {
       input = {
         keys = {
           ['<Esc>'] = { 'close', mode = 'i' },
+          ['<C-h>'] = { 'list_down', mode = { 'i', 'n' } },
+          ['<C-l>'] = { 'list_up', mode = { 'i', 'n' } },
           ['<C-k>'] = { 'preview_scroll_up', mode = { 'i', 'n' } },
           ['<C-j>'] = { 'preview_scroll_down', mode = { 'i', 'n' } },
         },
