cc1d7d000e4319e388470e1a2e4d944138a4259b
tewe <leventewe@gmail.com>
2026-09-05 12:00:32 +0200

    wezterm fix

--- home/programs/wezterm.nix
+++ home/programs/wezterm.nix
@@ -26,6 +26,16 @@ in
     package = wezterm-nixgl;
     settings = {
       color_scheme = "Catppuccin Latte";
+
+      # X11/i3 respects terminal cell increments when resizing.
+      use_resize_increments = true;
+      window_padding = {
+        left = 0;
+        right = 0;
+        top = 0;
+        bottom = 0;
+      };
+
       ssh_domains = [
         {
           name = "teweora";
